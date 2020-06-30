4a4019cf73d502d7ce7f9a1e0b8dcd871014fa54
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-30 16:45:09 -0400

    Update the terraform code to use molecule-iam-user-tf-module

---
+++ terraform/providers.tf
@@ -0,0 +1,7 @@
+# This is the "default" provider that is used assume the roles in the
+# other providers.  It uses the credentials of the caller.  It is also
+# used to assume the roles required to access remote state in the
+# Terraform backend.
+provider "aws" {
+  region = var.aws_region
+}
