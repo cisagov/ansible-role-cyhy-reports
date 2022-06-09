b84657b6a8558240765eb5571204c7ab95ccbf6d
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-06-09 00:03:08 -0400

    Update the molecule test for pip packages
    The host.pip_package class has been deprecated so we need to switch to
    the host.pip class. Additionally we should set the path for pip since
    we explicitly set it for the pip module in the role.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -39,7 +39,7 @@ def test_apt_packages(host, pkg):
 @pytest.mark.parametrize("pkg", ["cyhy-reports"])
 def test_pip_packages(host, pkg):
     """Test that the pip packages were installed."""
-    assert pkg in host.pip_package.get_packages()
+    assert pkg in host.pip.get_packages(pip_path="/usr/bin/pip2")
 
 
 @pytest.mark.parametrize(
